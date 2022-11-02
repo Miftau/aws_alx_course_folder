081ed2c39cb5fe5283356a600ecb11eabd8d4a25
Miftau <miftaudeenb@gmail.com>
2022-11-02 09:24:49 +0100

    making a distribution package

---
+++ distributionpack/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name= 'distributions',
+      version= '1.0',
+      description= 'Normal Distribution',
+      packages= ['distribution'],
+      author= 'PROGRESS ICT MEDIA CONCEPTS',
+      zip_safe = False)
